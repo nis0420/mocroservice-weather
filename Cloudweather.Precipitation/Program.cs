abdc1d9081fa9dea01fb29ed74f67589a0165f4b
nischal.panwala <nischal.panwala@platinumcove.com>
2025-04-14 09:56:00 -0500

    Update project dependencies and remove HTTP profile from launch settings

--- Cloudweather.Precipitation/Program.cs
+++ Cloudweather.Precipitation/Program.cs
@@ -2,4 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+app.MapGet("/observation/{zip}", (string zip) => {
+    return $"The zip code is {zip}";
+});
+
 app.Run();

0de367fd28a409203d4314fbc63261fda23ab4f2
nischal.panwala <nischal.panwala@platinumcove.com>
2025-04-25 10:03:28 -0500

    Add Dockerfiles and build scripts for DataLoader, Precipitation, Report, and Temperature services

---
+++ Cloudweather.Temperature/build_and_push_ecr_image.sh
@@ -0,0 +1,23 @@
+set -e
+
+# Set variables
+AWS_PROFILE="weather-ecr-agent"
+AWS_REGION="us-east-1"
+ACCOUNT_ID="842206816393"
+REPO_NAME="cloud-weather-temperature"
+TAG="latest"
+IMAGE_NAME="$REPO_NAME:$TAG"
+ECR_URL="$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_NAME"
+
+# Authenticate Docker with ECR
+aws ecr get-login-password --region $AWS_REGION --profile $AWS_PROFILE | \
+    docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+
+# Build the Docker image
+docker build -f ./Dockerfile -t $IMAGE_NAME .
+
+# Tag the Docker image
+docker tag $IMAGE_NAME $ECR_URL
+
+# Push the Docker image to ECR
+docker push $ECR_URL
